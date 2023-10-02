a2992dae144e9a23f6eda33428447dbf36033a0b
jineshmehta16 <mehtajinesh11@gmail.com>
2023-10-02 18:27:16 +0530

    prod issue sorted

--- src/appConfig.js
+++ src/appConfig.js
@@ -6,8 +6,8 @@ if (APP_CONFIG.ENV === 'DEV') {
   APP_CONFIG.API_URL = 'http://sanralpharma.com/webservices/public';
   APP_CONFIG.REPORTS_URL = 'http://sanralpharma.com'
 } else if (APP_CONFIG.ENV === 'PROD') {
-  APP_CONFIG.API_URL = 'http://hyp.techpromiot.com//webservices/public';
-  APP_CONFIG.REPORTS_URL = 'http://hyp.techpromiot.com'
+  APP_CONFIG.API_URL = 'https://hyp.techpromiot.com/webservices/public';
+  APP_CONFIG.REPORTS_URL = 'https://hyp.techpromiot.com'
 } else {
   APP_CONFIG.API_URL = 'http://sanralpharma.com/webservices/public';
   APP_CONFIG.REPORTS_URL = 'http://sanralpharma.com'
